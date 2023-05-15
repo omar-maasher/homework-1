377a4f4b58c09546eaf65a1ee066cd3c98accc10
omar-maasher <omar15448@gmail.com>
2023-05-15 18:37:56 +0300

    f1

---
+++ rev omar/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rev_omar.Models;
+using System.Diagnostics;
+
+namespace rev_omar.Controllers;
+
+
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Reverse(string n1)
+    {
+        
+        
+            
+        if(n1 == null)
+        {
+            ViewBag.bag = "123";
+
+
+        }
+        else 
+        {
+            string rearay = "";
+
+            for (int i = n1.Length - 1; i >= 0; i--)
+            {
+                rearay += n1[i];
+
+            }
+
+             ViewBag.bag = rearay;
+
+
+        }
+        return View("Index");
+
+    }
+
+}
+
+
---
+++ rev omar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace rev_omar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string n1  { get; set; }
+    }
+    
+}
